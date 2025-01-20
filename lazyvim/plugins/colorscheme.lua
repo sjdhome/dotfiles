264ca54cadaf41f9c644eee3bb499faaa3d32723
sjdhome <sjdhoome@gmail.com>
2025-01-20 20:00:18 +0800

    Add LazyVim

---
+++ lazyvim/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard"
+    }
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  }
+}
